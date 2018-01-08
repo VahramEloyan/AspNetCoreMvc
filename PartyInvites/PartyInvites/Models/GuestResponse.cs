178871cf7b0b8b3be7ea8310751f45706b478d73
vahrameloyan <vahrameloyan@gmail.com>
2018-01-08 15:49:28 +0400

    Start Rsvp Project

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public bool? WillAttend { set; get; }
+    }
+}
