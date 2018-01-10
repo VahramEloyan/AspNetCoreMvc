8b5caaa2e7624b4f3d46ce4baade55344baa7ab1
vahrameloyan <vahrameloyan@gmail.com>
2018-01-10 17:46:12 +0400

    Added Required attributes

--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="Please Enter Your Name")]
         public string Name { set; get; }
+        [Required(ErrorMessage = "Please Enter Your Email")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="please enter your valid email address")]
         public string Email { set; get; }
+        [Required(ErrorMessage = "Please Enter Your Phone")]
         public string Phone { set; get; }
+        [Required(ErrorMessage = "Please Enter Yes or NO")]
         public bool? WillAttend { set; get; }
 
     }
